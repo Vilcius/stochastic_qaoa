e856c071e9bd54ade4b6229012c04742e94f5ecc
Vilcuis <vilcius@protonmail.com>
2023-01-20 18:07:49 -0500

    Added some TODOs.

--- squbo_qaoa.py
+++ squbo_qaoa.py
@@ -4,6 +4,8 @@
 # author: Anthony Wilkie
 
 # TODO: figure out why QAOA no longer give correct solutions
+# TODO: implement the constraint that E(s,x_i) = E(s,x_j)
+# TODO: look for better way to implement basis encoding (W state)
 
 # %% Imports
 import pennylane as qml
@@ -14,8 +16,10 @@ from matplotlib import pyplot as plt
 # %% Create QUBO Problem Matrix
 linear = np.array(
         [[1.0, -3.0],
+         [1.0, 0.0],
          [1.0, 5.0],
-         [1.0, 0.0]])
+         [2.0, 5.0]#,
+         ])
 quadratic = np.array(
         [[0.0, 2.0],
          [0.0, 0.0]])
@@ -95,8 +99,8 @@ def get_qml_ising(linear, quadratic):
     hamiltonian = qml.Hamiltonian(h_coeff, h_obs)
     return hamiltonian
 
-C = [get_qml_ising(linear[i], quadratic) for i in range(3)]
-for i in range(3):
+C = [get_qml_ising(linear[i], quadratic) for i in range(len(linear))]
+for i in range(len(linear)):
     print('The Hamiltonians in each scenario')
     print(f'H_{i} =\n{C[i]}\n')
 
@@ -117,9 +121,9 @@ def to_ctrl_val(i, n):
         ctrl_val.append(int(s))
     return ctrl_val
 
-for i in range(3):
-    print(to_ctrl_val(2**i, 3))
-    print(to_ctrl_val(i,int(np.ceil(np.log2(3)))))
+for i in range(len(linear)):
+    print(to_ctrl_val(2**i, len(linear)))
+    print(to_ctrl_val(i,int(np.ceil(np.log2(len(linear))))))
 
 
 # %% Wires
@@ -145,9 +149,12 @@ def create_wires(encoding='basis'):
 
     return var, scen
 
-var, scen = create_wires('amplitude')
+encoding = 'basis'
+# encoding = 'amplitude'
 
+var, scen = create_wires(encoding)
 sdev = qml.device("default.qubit", wires=var+scen, shots=1)
+
 print(sdev.wires)
 print(dict(enumerate(var)))
 
@@ -199,7 +206,7 @@ def circuit(gammas, betas, encoding='basis', sample=False, n_layers=1):
         qml.Hadamard(wires=x)
         
     # initialize the scenario qubits
-    probs = np.array([1/n_scenarios,1/n_scenarios,1/n_scenarios])
+    probs = np.array([1/n_scenarios for _ in range(n_scenarios)])
     normalized = probs / np.linalg.norm(probs)
 
     if encoding == 'basis':
@@ -271,7 +278,7 @@ def sqaoa(n_layers=1, encoding='basis' ):
     counts = np.bincount(np.array(bit_strings))
     most_freq_bit_string = np.argmax(counts)
     print("Optimized (gamma, beta) vectors:\n{}".format(params[:, :n_layers]))
-    print("Most frequently sampled bit string is: {:03b} with probability {:.4f}".format(most_freq_bit_string,counts[most_freq_bit_string]/n_samples))
+    print(f"Most frequently sampled bit string is: {most_freq_bit_string:0{len(var)+len(scen)}b} with probability {counts[most_freq_bit_string]/n_samples:.4f}")
 
     return params, bit_strings
 
@@ -350,13 +357,6 @@ def graph(bitstrings, beamer):
 
 
 # %% Run the Thing
-n_scenarios, n_wires = np.shape(linear)
-encoding = 'basis'
-# encoding = 'amplitude'
-
-var, scen = create_wires(encoding)
-sdev = qml.device("default.qubit", wires=var+scen, shots=1)
-
 params, bitstrings = sqaoa(n_layers=1, encoding=encoding)
 parsed = parse_scenraio(bitstrings, encoding=encoding)
 graph(parsed, beamer=True)
@@ -366,5 +366,17 @@ print_obj_vals(n_wires, n_scenarios)
 # %% graphs
 # params, bitstrings = sqaoa(n_layers=1)
 # bitstrings = parse_scenraio(bitstrings)
-# graph(bitstrings, beamer=True)
+graph(parsed, beamer=True)
 
+# %% test
+for i in bitstrings:
+    print(f'{i:0{n_wires+len(scen)}b}')
+
+print('------------------------------------------------------------')
+print('parsed')
+print(parsed)
+
+# %% testt
+print(sum(C))
+print(n_scenarios)
+print(qml.draw_mpl(circuit, style='solarized_dark')(params[0],params[1],encoding))
